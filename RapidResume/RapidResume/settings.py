46bc7b9d7a72bcd3e8598f8012182b398ec04159
Eufracio <eufraciochavez327@yahoo.com>
2023-10-02 21:26:05 -0700

    feat(prod): modifying settings for production, added gunicorn

--- RapidResume/RapidResume/settings.py
+++ RapidResume/RapidResume/settings.py
@@ -14,8 +14,6 @@ from pathlib import Path
 from dotenv import load_dotenv, find_dotenv
 import os
 
-load_dotenv(find_dotenv())
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent
 
@@ -27,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
