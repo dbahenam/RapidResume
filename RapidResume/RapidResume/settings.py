8eaa55d799fd3d58d27a053d22ede784054e99b1
David Bahena Moctezuma <62975222+dbahenam@users.noreply.github.com>
2023-10-04 15:16:29 -0700

    changes to settings.py for csrf verification

--- RapidResume/RapidResume/settings.py
+++ RapidResume/RapidResume/settings.py
@@ -25,9 +25,11 @@ BASE_DIR = Path(__file__).resolve().parent
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
+
+CSRF_TRUSTED_ORIGINS = ['https://rapid-resume-f0k9.onrender.com','https://*.127.0.0.1']
 
 
 # Application definition
